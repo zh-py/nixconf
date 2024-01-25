5f65637140c80626d76dc938f0571ea9129222a9
zheng-py <pierrez1984@gmail.com>
2024-01-25 23:39:24 +0300

    second

--- flake.nix
+++ flake.nix
@@ -1,7 +1,6 @@
 {
   description = "my dotfiles";
 
-
   inputs = {
     # unstable has the 'freshest' packages you will find, even the AUR
     # doesn't do as good as this, and it's all precompiled.
@@ -10,24 +9,28 @@
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+    # mac-app-util.url = "github:hraban/mac-app-util";
 
-    nixvim = {
-      url = "github:nix-community/nixvim";
-      # If you are not running an unstable channel of nixpkgs, select the corresponding branch of nixvim.
-      # url = "github:nix-community/nixvim/nixos-23.05";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
+    #nixvim = {
+    #  url = "github:nix-community/nixvim";
+    # If you are not running an unstable channel of nixpkgs, select the corresponding branch of nixvim.
+    # url = "github:nix-community/nixvim/nixos-23.05";
+    #  inputs.nixpkgs.follows = "nixpkgs";
+    #};
   };
 
-  outputs = { nixpkgs, home-manager, ... }:
-	let
-	  system = "x86_64-darwin";
-	  pkgs  = nixpkgs.legacyPackages.${system};
-	in {
-      homeConfigurations.py = home-manager.lib.homeManagerConfiguration {
-		inherit pkgs;
-        modules = [ ./home.nix ];
-      };
+  outputs = {
+    nixpkgs,
+    home-manager,
+    ...
+  }: let
+    system = "x86_64-darwin";
+    pkgs = nixpkgs.legacyPackages.${system};
+  in {
+    formatter.${system} = pkgs.alejandra;
+    homeConfigurations.py = home-manager.lib.homeManagerConfiguration {
+      inherit pkgs;
+      modules = [./home.nix];
     };
-
+  };
 }
--- home.nix
+++ home.nix
@@ -1,6 +1,12 @@
-{ config, pkgs, ... }:
-
 {
+  config,
+  pkgs,
+  lib,
+  ...
+}: let
+  inherit (pkgs.lib) mkIf optionals;
+  inherit (pkgs.stdenv) isLinux isDarwin;
+in {
   home.username = "py";
   home.homeDirectory = "/Users/py";
 
@@ -18,9 +24,9 @@
   home.packages = with pkgs; [
     aria
     vim
-    neovim
-    nomacs
-    git
+    alacritty
+    thefuck
+    nerdfonts
 
     # # It is sometimes useful to fine-tune packages, for example, by applying
     # # overrides. You can do that directly here, just don't forget the
@@ -35,7 +41,6 @@
     #   echo "Hello, ${config.home.username}!"
     # '')
   ];
-
 
   # Home Manager is pretty good at managing dotfiles. The primary way to manage
   # plain files is through 'home.file'.
@@ -73,8 +78,157 @@
 
   # Let Home Manager install and manage itself.
   programs.home-manager.enable = true;
-  programs.zsh.enable = true;
-  programs.nixvim = {
-    plugins.lightline.enable = true;
+  nixpkgs.config = {
+    allowUnfree = true;
+    allowUnfreePredicate = _: true;
   };
+  programs.git = {
+    enable=true;
+    userEmail="pierrez1984@gmail.com";
+    userName="zheng-py";
+  };
+
+  programs.neovim = {
+    enable = true;
+    extraConfig = ''
+      colorscheme gruvbox
+      syntax enable
+             set mouse=a
+             set number
+             set wrap
+             set linebreak
+             set clipboard=unnamed
+             set nu rnu
+             let g:context_nvim_no_redraw = 1
+             let &scrolloff = 5
+             let g:context_enabled = 0
+             lua require'nvim-lastplace'.setup{}
+    '';
+    plugins = with pkgs.vimPlugins; let
+    in [
+      nvim-lastplace
+      nvim-surround
+      gruvbox
+      formatter-nvim
+      fzf-vim
+      context-vim
+      vim-airline
+      vim-nix
+      nerdcommenter
+    ];
+    viAlias = true;
+    withPython3 = true;
+  };
+
+  programs.zsh = {
+    enable = true;
+    enableAutosuggestions = true;
+    enableCompletion = true;
+    shellAliases = {
+      ll = "ls -l";
+    };
+    oh-my-zsh = {
+      enable = true;
+      theme = "robbyrussell";
+      plugins = [
+        "git"
+        "thefuck"
+        "z"
+        "command-not-found"
+        "poetry"
+        "sudo"
+        "terraform"
+        "systemadmin"
+        "vi-mode"
+      ];
+    };
+  };
+
+  programs.alacritty = {
+    enable = true;
+    settings = {
+      env = {
+        "TERM" = "xterm-256color";
+      };
+
+      window = {
+        #padding.x = 10;
+        #padding.y = 10;
+        dynamic_padding = true;
+        opacity = 0.95;
+        decorations = "buttonless";
+        startup_mode = "Maximized";
+      };
+
+      font = {
+        size = 16.0;
+        normal.family = "FiraCode Nerd Font";
+        bold.family = "FiraCode Nerd Font";
+        italic.family = "FiraCode Nerd Font";
+      };
+
+      cursor = {
+        style.shape = "Beam";
+        style.blinking = "On";
+      };
+
+      shell = {
+        program = "zsh";
+        args = [
+          "-l"
+        ];
+      };
+
+      colors = {
+        primary = {
+          background = "0x1b182c";
+          foreground = "0xcbe3e7";
+        };
+
+        normal = {
+          black = "0x100e23";
+          red = "0xff8080";
+          green = "0x95ffa4";
+          yellow = "0xffe9aa";
+          blue = "0x91ddff";
+          magenta = "0xc991e1";
+          cyan = "0xaaffe4";
+          white = "0xcbe3e7";
+        };
+
+        bright = {
+          black = "0x565575";
+          red = "0xff5458";
+          green = "0x62d196";
+          yellow = "0xffb378";
+          blue = "0x65b2ff";
+          magenta = "0x906cff";
+          cyan = "0x63f2f1";
+          white = "0xa6b3cc";
+        };
+      };
+    };
+  };
+
+  #home.activation = mkIf pkgs.stdenv.isDarwin {
+  #copyApplications = let
+  #apps = pkgs.buildEnv {
+  #name = "home-manager-applications";
+  #paths = config.home.packages;
+  #pathsToLink = "/Applications";
+  #};
+  #in
+  #lib.hm.dag.entryAfter ["writeBoundary"] ''
+  #baseDir="$HOME/Applications/Home Manager Apps"
+  #if [ -d "$baseDir" ]; then
+  #rm -rf "$baseDir"
+  #fi
+  #mkdir -p "$baseDir"
+  #for appFile in ${apps}/Applications/*; do
+  #target="$baseDir/$(basename "$appFile")"
+  #$DRY_RUN_CMD cp ''${VERBOSE_ARG:+-v} -fHRL "$appFile" "$baseDir"
+  #$DRY_RUN_CMD chmod ''${VERBOSE_ARG:+-v} -R +w "$target"
+  #done
+  #'';
+  #};
 }
